9bbca4b582e86f24edc47abe2605c0803b1b9592
Murali Allada <murali44@users.noreply.github.com>
2014-05-28 12:45:36 -0400

    delete dead code

--- MFRC522.py
+++ MFRC522.py
@@ -7,7 +7,6 @@ import sys
 sys.path.insert(0, '/CodeBase/Sonos_RFID_Controller/SoCo')
 import soco
 import subprocess
-sys.path.insert(0, '/CodeBase/Sonos_RFID_Controller/xmltodict')
 import json
   
 class MFRC522:
@@ -377,23 +376,6 @@ class MFRC522:
     self.AntennaOn()
 
 
-
-# Recursively follow redirects until there isn't a location header
-def resolve_http_redirect(url, depth=0):
-    if depth > 10:
-        raise Exception("Redirected "+depth+" times, giving up.")
-    o = urlparse.urlparse(url,allow_fragments=True)
-    conn = httplib.HTTPConnection(o.netloc)
-    path = o.path
-    if o.query:
-        path +='?'+o.query
-    conn.request("HEAD", path)
-    res = conn.getresponse()
-    headers = dict(res.getheaders())
-    if headers.has_key('location') and headers['location'] != url:
-        return resolve_http_redirect(headers['location'], depth+1)
-    else:
-        return url
   
 continue_reading = True
 # Capture SIGINT
